102267ae0661fa3e660c36fa97e6e02cdfadeeb1
JonasMueller1991 <jonas.mueller@tngtech.com>
2022-12-07 09:01:39 +0100

    Refinement day 6

--- day_6/main.py
+++ day_6/main.py
@@ -5,20 +5,24 @@ NUM_DISTINCT_SIGNALS = 14
 code_segment = deque([], maxlen=NUM_DISTINCT_SIGNALS)
 
 
+def count_method_call(func):
+    def wrapper(*args, **kwargs):
+        wrapper.calls += 1
+        return func(*args, **kwargs)
+
+    wrapper.calls = 0
+    return wrapper
+
+
+@count_method_call
 def check_signal_to_end(signal: str) -> bool:
-    code_segment.popleft()
     code_segment.append(signal)
     return len(set(code_segment)) == NUM_DISTINCT_SIGNALS
 
 
 if __name__ == '__main__':
-    result_counter = 0
     with open('input.csv', 'r') as file:
-        for counter, c in enumerate(file.read()):
-            if counter < NUM_DISTINCT_SIGNALS:
-                code_segment.append(c)
-            elif check_signal_to_end(c):
-                result_counter = counter + 1
+        for c in file.read():
+            if check_signal_to_end(c):
                 break
-
-    print(f'Processed signals: {result_counter}')
+    print(f'Processed signals: {check_signal_to_end.calls}')
